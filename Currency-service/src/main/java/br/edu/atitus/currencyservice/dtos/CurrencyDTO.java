fcfb1a59be9c412fdde31325fbe9930f12b5fa50
JeanCDemarchi <jenaosei1@gmail.com>
2026-05-21 20:27:15 -0300

    Delete Currency-service directory

--- Currency-service/src/main/java/br/edu/atitus/currencyservice/dtos/CurrencyDTO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.atitus.currencyservice.dtos;
-
-public record CurrencyDTO(
-
-        String sourceCurrency,
-        String targetCurrency,
-        Double conversionRate,
-        String environment) {
-}
